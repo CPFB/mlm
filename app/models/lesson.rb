5b1513f4c22735b3164895c327399e5cd57af4de
Christopher Fortenberry <cpfortenberry@gmail.com>
2011-04-26 23:31:30 -0500

    deleted after_create call for now

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,24 +5,6 @@ class Lesson < ActiveRecord::Base
   validates_presence_of :student, :datetime, :charge
   validates_numericality_of :charge, :greater_than_or_equal_to => 0
   
-  # def set_balance
-  #   balance = charge
-  #   if (self.student.lessons_with_balance_due.size == 0 && self.student.payments_with_balance_remaining.size > 0)
-  #     for payment in self.student.payments_with_balance_remaining
-  #       if balance >= payment.balance
-  #         balance -= payment.balance
-  #         payment.balance = 0
-  #       else
-  #         payment.balance -= balance
-  #         balance = 0
-  #       end
-  #       payment.save
-  #     end
-  #   end
-  # end
-  
-  after_create :set_balance
-
   def set_balance
     lesson_balance = charge
     for payment in self.student.payments_with_balance_remaining
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,25 +3,7 @@ class Payment < ActiveRecord::Base
   belongs_to :instructor
   
   validates_presence_of :student, :instructor, :date, :amount, :payment_type
-  
-  # def set_balance
-  #   balance = self.amount
-  #   if self.student.lessons_with_balance_due.size > 0
-  #     for lesson in self.student.lessons_with_balance_due
-  #       if balance >= lesson.balance
-  #         balance -= lesson.balance
-  #         lesson.balance = 0
-  #       else
-  #         lesson.balance -= balance
-  #         balance = 0
-  #       end
-  #       lesson.save
-  #     end
-  #   end
-  # end
-  
-  after_create :set_balance
-  
+    
   def set_balance
     payment_balance = amount
     for lesson in student.lessons_with_balance_due
